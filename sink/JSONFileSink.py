b03baef6b224e0d2b41e683a9c85334a6aba8c29
ZackDev <bastian.maier@gmx.de>
2021-08-24 20:36:12 +0200

    added exception handling to JSONFileSink

--- sink/JSONFileSink.py
+++ sink/JSONFileSink.py
@@ -5,5 +5,18 @@ import json
 class JSONFileSink(AbstractSink):
 
     def _store(self):
-        with open(self.target, 'w') as file:
-            file.write(json.dumps(self.data))
+        json_data = None
+
+        try:
+            json_data = json.dumps(self.data)
+        except:
+            pass
+
+        if json_data != None:
+            try:
+                with open(self.target, 'w') as file:
+                    file.write(json_data)
+            except Exception as e:
+                raise Exception(e)
+        else:
+            raise Exception('JSONFileSink: json_data is None')
