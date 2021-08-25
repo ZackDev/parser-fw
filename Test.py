e117ad226da4dd5b3feb5598c4daa47932270d41
ZackDev <bastian.maier@gmx.de>
2021-08-25 16:32:27 +0200

    added JSONTest.py for basic json module

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 import unittest
 from test.ValidatorsTest import ValidatorsTest
+from test.JSONTest import JSONTest
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test/JSONTest.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+
+class JSONTest(unittest.TestCase):
+
+    def test_jsonDumps(self):
+        with self.subTest():
+            self.assertEqual(json.dumps('{}'), '"{}"')
+
+        with self.subTest():
+            self.assertNotEqual(json.dumps('{}'), '')
+
+        with self.subTest():
+            self.assertEqual(json.dumps(''), '""')
+
+        with self.subTest():
+            self.assertEqual(json.dumps(None), 'null')
+
+        with self.subTest():
+            self.assertEqual(json.dumps('"{/}"'), '""')
+
+        with self.subTest():
+            self.assertEqual(json.dumps('"{/}"'), '""')
+
+        with self.subTest():
+            with self.assertRaises(json.JSONDecodeError):
+                json.loads('"{/}"')
