094a9440c1be66a8f7fd3c40910bdea0660f3ed8
ZackDev <bastian.maier@gmx.de>
2021-08-25 16:33:57 +0200

    finegrained testing using subTest()

--- test/ValidatorsTest.py
+++ test/ValidatorsTest.py
@@ -3,18 +3,24 @@ from parser.Validators import strToInteger
 
 class ValidatorsTest(unittest.TestCase):
 
-    def testStrToInteger(self):
-        self.assertEqual(strToInteger('123'), 123)
+    def test_strToInteger(self):
+        with self.subTest():
+            self.assertEqual(strToInteger('123'), 123)
 
-        self.assertEqual(strToInteger('-123', '-'), -123)
+        with self.subTest():
+            self.assertEqual(strToInteger('-123', '-'), -123)
 
-        self.assertEqual(strToInteger('999999999'), 999999999)
+        with self.subTest():
+            self.assertEqual(strToInteger('999999999'), 999999999)
 
-        with self.assertRaises(ValueError):
-            strToInteger('123', '-')
+        with self.subTest():
+            with self.assertRaises(ValueError):
+                strToInteger('123', '-')
 
-        with self.assertRaises(TypeError):
-            strToInteger(123)
+        with self.subTest():
+            with self.assertRaises(TypeError):
+                strToInteger(123)
 
-        with self.assertRaises(ValueError):
-            strToInteger('abcd')
+        with self.subTest():
+            with self.assertRaises(ValueError):
+                strToInteger('abcd')
