8fcfcddd9d557af69096a6ee7b7dbad776fc0d58
ZackDev <bastian.maier@gmx.de>
2021-08-27 11:24:29 +0200

    added logging to HTTPResponseSource

--- source/HTTPResponseSource.py
+++ source/HTTPResponseSource.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABC
 from abstract.AbstractSource import AbstractSource
 import requests
@@ -5,11 +6,20 @@ import requests
 class HTTPResponseSource(AbstractSource):
 
     def _get_data(self):
+        logging.debug('AbstractSource: _get_data() called.')
         try:
+            logging.debug(f'calling requests.get() with {self.source}')
             rsp = requests.get(self.source)
             if rsp.status_code == 200:
+                logging.debug(f'call to requests.get() finished.')
+                logging.debug(f'status code: {rsp.status_code}')
+                logging.debug(f'content: {rsp.content}')
                 self.data = rsp.content
             else:
+                logging.critical('call to requests.get() failed.')
+                logging.critical(f'status code: {rsp.status_code}')
+                logging.critical(f'content: {rsp.content}')
                 raise ConnectionError(f'Connection failed with http status code {rsp.status_code}')
         except Exception as e:
+            logging.critical('call to requests.get() failed. Raising ConnectionError.')
             raise ConnectionError(e)
