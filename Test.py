a3666e8a799d06ab253048d97b6261aa6455b3f5
ZackDev <bastian.maier@gmx.de>
2021-08-28 17:52:38 +0200

    added test for HTTPResponseSource

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ import unittest
 from test.BaseModuleImportTest import ThirdPartyModuleImportTest
 from test.BaseModuleImportTest import FrameworkModuleImportTest
 from test.ValidatorsTest import ValidatorsTest
+from test.CustomModuleTest import HTTPResponseSourceTest
 from test.JSONTest import JSONTest
 
 if __name__ == '__main__':
---
+++ test/CustomModuleTest.py
@@ -0,0 +1,28 @@
+import unittest
+from source.HTTPResponseSource import HTTPResponseSource
+
+class HTTPResponseSourceTest(unittest.TestCase):
+    def test_url_response_success(self):
+        url = 'https://istandischeuernochimamt.de/'
+        source = HTTPResponseSource(url)
+
+        request_successfull = False
+
+        try:
+            source._get_data()
+            request_successfull = True
+        except:
+            request_successfull = False
+
+        self.assertEqual(request_successfull, True)
+
+
+    def test_url_response_failure(self):
+        def test_url_response_success(self):
+            url = 'not-a-valid-url'
+            source = HTTPResponseSource(url)
+
+            request_successfull = False
+            
+            with self.assertRaises(ConnectionError):
+                source._get_data()
