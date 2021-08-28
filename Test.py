b5cbc63a5e8b1899015f4b2c52be396d35748ad0
ZackDev <bastian.maier@gmx.de>
2021-08-28 16:32:04 +0200

    raised verbosity for tests

--- Test.py
+++ Test.py
@@ -5,4 +5,5 @@ from test.ValidatorsTest import ValidatorsTest
 from test.JSONTest import JSONTest
 
 if __name__ == '__main__':
-    unittest.main()
+    # verbosity determines the output of a test run, 2 seems to be the highest
+    unittest.main(verbosity=2)
