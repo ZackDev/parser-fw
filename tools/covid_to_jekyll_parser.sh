0d89ab7f44260e36c2a6e34b135168366dbc962d
ZackDev <bastian.maier@gmx.de>
2021-08-31 16:35:00 +0200

    added case where file doesn't exists at target directory

--- tools/covid_to_jekyll_parser.sh
+++ tools/covid_to_jekyll_parser.sh
@@ -13,27 +13,38 @@ if [ $num_args -eq 3 ]; then
   cd $script_base_dir
   python3 InfoGetSetPrototype.py -s $1 -l $3
   ecode=$?
+  moved_file=false
   if [ $ecode -eq 0 ]; then
     if [ -f $2 ]; then
       cd $jekyll_base_dir
       git pull
       cd $script_base_dir
-      ./tools/diff_files.sh $2 $jekyll_base_dir$target_dir$2
-      ecode=$?
-      if [ $ecode -eq 0 ]; then
+      # case: file at jekyll dir exists, compare hashes
+      if [ -f $jekyll_base_dir$target_dir$2 ]; then
+        ./tools/diff_files.sh $2 $jekyll_base_dir$target_dir$2
+        ecode=$?
+        if [ $ecode -eq 0 ]; then
+          mv $2 $jekyll_base_dir$target_dir
+          moved_file=true
+        fi
+      # case: file at jekyll dir DOESN'T exist
+      elif [ ! -f $jekyll_base_dir$target_dir$2 ]; then
         mv $2 $jekyll_base_dir$target_dir
+        moved_file=true
+      fi
+      if [ "$moved_file" = true ]; then
         cd $jekyll_base_dir
         git add ./$target_dir$2
         git commit -m "automated dataset update"
         git push
         echo "copied output file to jekyll directory. set git commit message."
       else
-        echo "files are equal. removing artifact."
+        echo "file wasn't moved. deleting file."
         cd $script_base_dir
         rm $2
       fi
     else
-      echo "expected file not found"
+      echo "expected file not found."
       exit 1
     fi
   else
@@ -41,9 +52,7 @@ if [ $num_args -eq 3 ]; then
     exit 1
   fi
 else
-  echo "error: script takes two arguments:"
-  echo "1) name of the sequence"
-  echo "2) name of the output file"
+  echo "invalid number of arguments."
   exit 1
 fi
 
