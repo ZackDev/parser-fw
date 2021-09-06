7525c3e3ebde75e98b56b9c5d8c7d39faa00b07c
ZackDev <bastian.maier@gmx.de>
2021-09-06 20:48:21 +0200

    added booster vaccinations

--- parser/DailyVaccinationsGithubParser.py
+++ parser/DailyVaccinationsGithubParser.py
@@ -20,9 +20,11 @@ class DailyVaccinationsGithubParser(AbstractParser):
             dates = []
             primary_vaccinations = []
             secondary_vaccinations = []
+            booster_vaccinations = []
 
             tmp_pri_vacc = 0
             tmp_sec_vacc = 0
+            tmp_booster_vacc = 0
             index = 0
             date = None
 
@@ -38,22 +40,28 @@ class DailyVaccinationsGithubParser(AbstractParser):
                         dates.append(date)
                         primary_vaccinations.append(tmp_pri_vacc)
                         secondary_vaccinations.append(tmp_sec_vacc)
+                        booster_vaccinations.append(tmp_booster_vacc)
                         tmp_pri_vacc = 0
                         tmp_sec_vacc = 0
+                        tmp_booster_vacc = 0
                     else:
                         vacc_series = strToInteger(line[3], '+')
                         if vacc_series == 1:
                             tmp_pri_vacc += strToInteger(line[4], '+')
                         elif vacc_series == 2:
                             tmp_sec_vacc += strToInteger(line[4], '+')
+                        elif vacc_series == 3:
+                            tmp_booster_vacc += strToInteger(line[4], '+')
+                        else:
+                            self.logger.warn(f'unknown vacc_series: {vacc_series}')
                 index+=1
 
             ''' check data for consistency, equal amount of dates and cases '''
-            if len(dates) != len(primary_vaccinations) != len(secondary_vaccinations):
+            if len(dates) != len(primary_vaccinations) != len(secondary_vaccinations) != len(booster_vaccinations):
                 raise DataLengthUnequalError('dates, primary_vaccinations and secondary_vaccinations array length not equal.')
 
-            if len(dates) < 1 and len(primary_vaccinations) < 1 and len(secondary_vaccinations):
+            if len(dates) < 1 and len(primary_vaccinations) < 1 and len(secondary_vaccinations) and len(booster_vaccinations) < 1:
                 raise DataLengthZeroError('dates, primary_vaccinations and secondary_vaccinations array length is zero.')
 
-            dict = { 'dates':dates, 'primary_vaccinations':primary_vaccinations, 'secondary_vaccinations':secondary_vaccinations }
+            dict = { 'dates':dates, 'primary_vaccinations':primary_vaccinations, 'secondary_vaccinations':secondary_vaccinations, 'booster_vaccinations':booster_vaccinations }
             self.parsed_data = dict
