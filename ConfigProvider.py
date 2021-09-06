727f02a5eff00140502d2202bdef3113f65f1d6d
ZackDev <bastian.maier@gmx.de>
2021-09-06 16:23:44 +0200

    refactoring

--- ConfigProvider.py
+++ ConfigProvider.py
@@ -8,93 +8,158 @@ import logging
 logger = logging.getLogger(__name__)
 _CONFIG_DIRECTORY = './config/'
 
+class ConfigProviderError(Exception):
+    pass
+
+
 class ConfigProvider(AbstractConfigProvider):
     def __init__(self, config_name):
         super().__init__(config_name)
         self.cfg = get_config(config_name)
+        if self.cfg is None:
+            raise ConfigProviderError(ValueError)
+
 
     def get_source_class(self):
-        source_cls = get_class(f"{self.cfg['source']['package']}.{self.cfg['source']['module']}", self.cfg['source']['class'])
-        return source_cls
+        try:
+            source_cls = get_class(f"{self.cfg['source']['package']}.{self.cfg['source']['module']}", self.cfg['source']['class'])
+            return source_cls
+        except ModuleNotFoundError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except AttributeError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+
 
     def get_parser_class(self):
-        parser_cls = get_class(f"{self.cfg['parser']['package']}.{self.cfg['parser']['module']}", self.cfg['parser']['class'])
-        return parser_cls
+        try:
+            parser_cls = get_class(f"{self.cfg['parser']['package']}.{self.cfg['parser']['module']}", self.cfg['parser']['class'])
+            return parser_cls
+        except ModuleNotFoundError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except AttributeError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+
 
     def get_sink_class(self):
-        sink_cls = get_class(f"{self.cfg['sink']['package']}.{self.cfg['sink']['module']}", self.cfg['sink']['class'])
-        return sink_cls
+        try:
+            sink_cls = get_class(f"{self.cfg['sink']['package']}.{self.cfg['sink']['module']}", self.cfg['sink']['class'])
+            return sink_cls
+        except ModuleNotFoundError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except AttributeError as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'{e}')
+            raise ConfigProviderError(e)
+
 
     def get_source_parameters(self):
-        source_params = get_parameters(self.cfg, 'source')
-        return source_params
+        try:
+            source_params = get_parameters(self.cfg, 'source')
+            return source_params
+        except AttributeError as e:
+            logger.debug(f'get_source_parameters(): {e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'get_source_parameters(): {e}')
+            raise ConfigProviderError(e)
+
 
     def get_parser_parameters(self):
-        parser_params = get_parameters(self.cfg, 'parser')
-        return parser_params
+        try:
+            parser_params = get_parameters(self.cfg, 'parser')
+            return parser_params
+        except AttributeError as e:
+            logger.debug(f'get_parser_parameters(): {e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'get_parser_parameters(): {e}')
+            raise ConfigProviderError(e)
+
 
     def get_sink_parameters(self):
-        sink_params = get_parameters(self.cfg, 'sink')
-        return sink_params
+        try:
+            sink_params = get_parameters(self.cfg, 'sink')
+            return sink_params
+        except AttributeError as e:
+            logger.debug(f'get_sink_parameters(): {e}')
+            raise ConfigProviderError(e)
+        except Exception as e:
+            logger.debug(f'get_sink_parameters(): {e}')
+            raise ConfigProviderError(e)
 
 
+'''
+imports and returns the module specified by <module_name>
+raises ModuleNotFoundError
+'''
 def _load_module(module_name):
     module = importlib.import_module(module_name)
     return module
 
 
+'''
+returns the class identified by <module_name> and <class_name>
+raises AttributeError
+'''
 def get_class(module_name, class_name):
-    module = None
-    cls = None
-    try:
-        module = _load_module(module_name)
-    except ModuleNotFoundError as e:
-        logger.critical(f'{e}')
-    if module:
-        try:
-            cls = getattr(module, class_name)
-        except AttributeError as e:
-            logger.critical(f'{e}')
+    module = _load_module(module_name)
+    cls = getattr(module, class_name)
     return cls
 
-
+'''
+returns a list of all files in the directory specified by _CONFIG_DIRECTORY
+'''
 def _get_config_files():
     config_files = [f for f in listdir(_CONFIG_DIRECTORY)
-                        if (isfile(join(_CONFIG_DIRECTORY, f))
-                        and f != 'ConfigReader.py'
-                        and f != '__init__.py'
-                        and f != 'ClassLoader.py')]
+                        if isfile(join(_CONFIG_DIRECTORY, f))]
     return config_files
 
 
+'''
+returns the config's file content specified by <name>
+'''
 def get_config(name):
     config_files = _get_config_files()
     for file in config_files:
-        try:
-            with open(join(_CONFIG_DIRECTORY, file), 'r') as f:
-                cfg = json.loads(f.read())
-                if cfg['name'] != None and cfg['name'] == name:
-                    return cfg
-        except Exception as e:
-            print(f'{file}: {e}')
-    return None
+        with open(join(_CONFIG_DIRECTORY, file), 'r') as f:
+            cfg = json.loads(f.read())
+            if cfg['name'] != None and cfg['name'] == name:
+                return cfg
 
-
+'''
+returns the json names field of all config files
+'''
 def get_config_names():
     names = []
     config_files = _get_config_files()
     for file in config_files:
-        try:
-            with open(join(_CONFIG_DIRECTORY, file), 'r') as f:
-                cfg = json.loads(f.read())
-                if cfg['name']:
-                    names.append(cfg['name'])
-        except Exception as e:
-            print(f'{file}: {e}')
+        with open(join(_CONFIG_DIRECTORY, file), 'r') as f:
+            cfg = json.loads(f.read())
+            if cfg['name']:
+                names.append(cfg['name'])
     return names
 
 
+'''
+returns the parameters specified by <cfg> and <sequence_part> as a dict
+'''
 def get_parameters(cfg, sequence_part):
     params = {}
-    params.update({p['name']:p['value'] for p in cfg[sequence_part]['parameters']})
+    try:
+        params.update({p['name']:p['value'] for p in cfg[sequence_part]['parameters']})
+    except:
+        pass
     return params
--- Seshu.py
+++ Seshu.py
@@ -1,121 +1,45 @@
 import argparse
 import logging
 import importlib
-from ConfigProvider import ConfigProvider
+from ConfigProvider import ConfigProvider, ConfigProviderError
 from Sequence import Sequence
 
 
 def init_sequence(sequence_name):
     logger = logging.getLogger(__name__)
-    test = False
 
-    cfg_provider = ConfigProvider(sequence_name)
+    try:
+        cfg_provider = ConfigProvider(sequence_name)
 
-    source_cls = cfg_provider.get_source_class()
-    source_params = cfg_provider.get_source_parameters()
-    source = source_cls(**source_params)
+        source_cls = cfg_provider.get_source_class()
+        source_params = cfg_provider.get_source_parameters()
+        source = source_cls(**source_params)
 
-    parser_cls = cfg_provider.get_parser_class()
-    parser_params = cfg_provider.get_parser_parameters()
-    parser_params.update({"source":source})
-    parser = parser_cls(**parser_params)
+        parser_cls = cfg_provider.get_parser_class()
+        parser_params = cfg_provider.get_parser_parameters()
+        parser_params.update({"source":source})
+        parser = parser_cls(**parser_params)
 
-    sink_cls = cfg_provider.get_sink_class()
-    sink_params = cfg_provider.get_sink_parameters()
-    sink_params.update({"parser":parser})
-    sink = sink_cls(**sink_params)
+        sink_cls = cfg_provider.get_sink_class()
+        sink_params = cfg_provider.get_sink_parameters()
+        sink_params.update({"parser":parser})
+        sink = sink_cls(**sink_params)
+    except ConfigProviderError as e:
+        logger.critical(f'error reading config: {e}')
+        logger.critical('exiting program due to critical error.')
+        exit(1)
+    except Exception as e:
+        logger.critical(f'unexcpected error reading config: {e}')
+        logger.critical('exiting program due to critical error.')
+        exit(1)
 
-    s = Sequence(source, parser, sink, sequence_name)
-    s.run()
-
-    if cfg and test == True:
-        '''
-        create source object from cfg
-        - get class_name from cfg
-        '''
-        package_name, module_name, class_name = None, None, None
-        try:
-            package_name = cfg['source']['package']
-            module_name = cfg['source']['module']
-            class_name = cfg['source']['class']
-        except KeyError as e:
-            logger.critical('class info (package, module, class) not found in config.')
-        except Exception as e:
-            logger.critical(f'unexpected error: {e}')
-
-        '''
-        - dynamic import of <class> from <package>.<module>
-        '''
-        if package_name and module_name and class_name:
-            try:
-                source = get_class(f'{package_name}.{module_name}', class_name)
-            except Exception as e:
-                logger.critical(f'unable to dynamically load class {class_name} from {package_name}.{module_name}.')
-        else:
-            if not package_name:
-                logger.critical('failed to get package_name from cfg.')
-            if not module_name:
-                logger.critical('falied to get module_name from cfg.')
-            if not class_name:
-                logger.critical('failed to get class_name from cfg.')
-
-        '''
-        - populate source object kwargs (keyword arguments) with parameters from cfg
-        '''
-        so_params = {}
-        try:
-            so_params.update({p['name']:p['value'] for p in cfg['source']['parameters']})
-        except KeyError as e:
-            pass
-        so = source(**so_params)
-
-        '''
-        create parser object from cfg
-        '''
-        package_name, module_name, class_name = None, None, None
-        try:
-            package_name = cfg['parser']['package']
-            module_name = cfg['parser']['module']
-            class_name = cfg['parser']['class']
-        except KeyError as e:
-            logger.critical('class info (package, module, class) not found in config.')
-        try:
-            parser = get_class(f'{package_name}.{module_name}', class_name)
-        except Exception as e:
-            logger.critical(f'unable to dynamically load class {class_name} from {package_name}.{module_name}.')
-        pa_params = {}
-        try:
-            pa_params.update({p['name']:p['value'] for p in cfg['parser']['parameters']})
-        except KeyError as e:
-            pass
-        pa_params.update({"source":so})
-        pa = parser(**pa_params)
-
-        '''
-        create sink object from cfg
-        '''
-        package_name, module_name, class_name = None, None, None
-        try:
-            package_name = cfg['sink']['package']
-            module_name = cfg['sink']['module']
-            class_name = cfg['sink']['class']
-        except KeyError as e:
-            logger.critical('sink classname not found in config.')
-        try:
-            sink = get_class(f'{package_name}.{module_name}', class_name)
-        except Exception as e:
-            logger.critical(f'unable to dynamically load class {class_name} from {package_name}.{module_name}.')
-        si_params = {}
-        try:
-            si_params.update({p['name']:p['value'] for p in cfg['sink']['parameters']})
-        except KeyError as e:
-            pass
-        si_params.update({"parser":pa})
-        si = sink(**si_params)
-
-        Sequence(so, pa, si, cfg['name']).run()
-    else:
-        print(f'sequence: {sequence} not found.\navailable sequences:\n{", ".join(get_config_names())}')
+    try:
+        s = Sequence(source, parser, sink, sequence_name)
+        s.run()
+    except Exception as e:
+        logger.critical(f'error during creation/running of sequence: {e}')
+        logger.critical('exiting program due to critical error.')
+        exit(1)
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
