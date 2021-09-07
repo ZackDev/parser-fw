51a31b7254a9f25889af7900591a026e5ffe83ff
ZackDev <bastian.maier@gmx.de>
2021-09-07 23:21:11 +0200

    removed comments

--- misc/Validators.py
+++ misc/Validators.py
@@ -2,12 +2,6 @@ from datetime import date, timedelta
 
 
 def is_valid_ISO8601_date(date_string):
-    """
-    takes <date_string> and checks if the format is valid according to ISO 8601
-    standard.
-    see https://en.wikipedia.org/wiki/ISO_8601 for further information
-    date_string is checked against Date format as specified in ISO 8601 (YYYY-MM-DD)
-    """
     try:
         date.fromisoformat(date_string)
         return True
@@ -30,13 +24,6 @@ def is_valid_ISO8601_date_array(date_array, strict=False):
         else:
             return False
 
-    """
-    takes <date_array>, an string array of dates and checks if the dates in it are
-    valid ISO8601 dates.
-    <strict> is a switch:
-        if set to True, checks if dates are ascending without gaps
-        if set to False, only the specific date at <date_array[n]> is checked
-    """
 
 def _build_date_array(start_date, length):
     date_array = []
