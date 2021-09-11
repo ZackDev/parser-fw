86f320b9e361cd32f89fb5338dc0123b3e2dc692
ZackDev <bastian.maier@gmx.de>
2021-09-11 13:52:40 +0200

    added example of how to call Seshu from python

---
+++ Client.py
@@ -0,0 +1,9 @@
+from Seshu import init_sequence, init_logger
+
+"""
+example: how to use Seshu from python
+"""
+
+if __name__ == '__main__':
+    init_logger(2)
+    init_sequence('daily_cases')
--- Seshu.py
+++ Seshu.py
@@ -41,26 +41,30 @@ def init_sequence(sequence_name):
         logger.critical('exiting program due to critical error.')
         exit(1)
 
+
+def init_logger(loglevel):
+    loglevel = loglevel*10
+    # loglevels in numbers: DEBUG:10, INFO:20, WARN:30, ERROR:40, CRITICAL:50
+    available_loglevels = [logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR, logging.CRITICAL]
+    cnt = available_loglevels.count(loglevel)
+    if cnt == 1:
+        logging.basicConfig(filename='parser-fw.log', encoding='utf-8', level=loglevel, format='%(asctime)s %(name)s %(levelname)s : %(message)s')
+        logger = logging.getLogger(__name__)
+        logger.info(f'program started with loglevel: {loglevel}')
+    else:
+        logging.basicConfig(filename='parser-fw.log', encoding='utf-8', format='%(asctime)s %(name)s %(levelname)s : %(message)s')
+        logger = logging.getLogger(__name__)
+        logger.warning(f'provided loglevel: {loglevel} not supported. running Seshu with default loglevel.')
+
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-s", "--sequence", type=str)
     arg_parser.add_argument("-l", "--loglevel", type=int)
     args = arg_parser.parse_args()
     if (args.loglevel and args.sequence):
-        loglevel = args.loglevel*10
-        # loglevels in numbers: DEBUG:10, INFO:20, WARN:30, ERROR:40, CRITICAL:50
-        available_loglevels = [logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR, logging.CRITICAL]
-        cnt = available_loglevels.count(loglevel)
-        if cnt == 1:
-            logging.basicConfig(filename='parser-fw.log', encoding='utf-8', level=loglevel, format='%(asctime)s %(name)s %(levelname)s : %(message)s')
-            logger = logging.getLogger(__name__)
-            logger.info(f'program started with loglevel: {loglevel}')
-            init_sequence(args.sequence)
-        else:
-            logging.basicConfig(filename='parser-fw.log', encoding='utf-8', format='%(asctime)s %(name)s %(levelname)s : %(message)s')
-            logger = logging.getLogger(__name__)
-            logger.warning(f'provided loglevel: {loglevel} not supported. running Seshu with default loglevel.')
-            init_sequence(args.sequence)
+        init_logger(args.loglevel)
+        init_sequence(args.sequence)
     else:
         arg_parser.print_help()
         exit(1)
