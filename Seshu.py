4eecffe0067aaf67c670f284b48a037d3cb9eadc
ZackDev <bastian.maier@gmx.de>
2021-09-14 00:50:15 +0200

    refactored init_sequence(...) and init_logger(...) to init(...)

--- Seshu.py
+++ Seshu.py
@@ -10,8 +10,17 @@ _DEFAULT_LOGLEVEL = 2
 class SeshuError:
     pass
 
-def init_sequence(sequence_name):
+
+def init(sequence_name, loglevel=_DEFAULT_LOGLEVEL):
+    loglevel = loglevel*10
+    # loglevels in numbers: DEBUG:10, INFO:20, WARN:30, ERROR:40, CRITICAL:50
+    available_loglevels = [logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR, logging.CRITICAL]
+    cnt = available_loglevels.count(loglevel)
+    if cnt != 1:
+        loglevel = logging.INFO
+    logging.basicConfig(filename='parser-fw.log', encoding='utf-8', level=loglevel, format='%(asctime)s %(name)s %(levelname)s : %(message)s')
     logger = logging.getLogger(__name__)
+    logger.info(f'program started with sequence_name: {sequence_name} and loglevel: {loglevel}.')
 
     """ create SequenceProvider """
     try:
@@ -31,25 +40,12 @@ def init_sequence(sequence_name):
         exit(0)
 
 
-def init_logger(loglevel=_DEFAULT_LOGLEVEL):
-    loglevel = loglevel*10
-    # loglevels in numbers: DEBUG:10, INFO:20, WARN:30, ERROR:40, CRITICAL:50
-    available_loglevels = [logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR, logging.CRITICAL]
-    cnt = available_loglevels.count(loglevel)
-    if cnt != 1:
-        loglevel = logging.INFO
-    logging.basicConfig(filename='parser-fw.log', encoding='utf-8', level=loglevel, format='%(asctime)s %(name)s %(levelname)s : %(message)s')
-    logger = logging.getLogger(__name__)
-    logger.info(f'program started with loglevel: {loglevel}')
-
-
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-s", "--sequence", type=str)
     arg_parser.add_argument("-l", "--loglevel", type=int, default=_DEFAULT_LOGLEVEL)
     args = arg_parser.parse_args()
     if (args.loglevel and args.sequence):
-        init_logger(args.loglevel)
-        init_sequence(args.sequence)
+        init(args.sequence, args.loglevel)
     else:
         arg_parser.print_help()
