843a02473b059620e817ea27fbaefb8feca9c68f
ZackDev <bastian.maier@gmx.de>
2021-09-15 13:58:44 +0200

    refactoring

--- steps/VaccinationsByVaccineParser.py
+++ steps/VaccinationsByVaccineParser.py
@@ -7,33 +7,33 @@ import csv
 class VaccinationsByVaccineParser(AbstractStep):
     def run(self, data):
         with StringIO(data.decode('utf-8')) as vaccine_csv:
+            csv_reader = csv.reader(vaccine_csv, delimiter=',')
             dict = None
-            moderna_count = 0
-            astrazeneca_count = 0
-            janssen_count = 0
-            comirnaty_count = 0
+            moderna_doses = 0
+            astrazeneca_doses = 0
+            janssen_doses = 0
+            comirnaty_doses = 0
 
             ''' NOTE: the first line is the header '''
-            csv_reader = csv.reader(vaccine_csv, delimiter=',')
             index = 0
             for line in csv_reader:
                 if index > 0:
                     vacc_name = line[2]
-                    vacc_count = line[4]
+                    vacc_doses = line[4]
                     try:
-                        vacc_count = str_to_integer(vacc_count, '+')
+                        vacc_doses = str_to_integer(vacc_doses, '+')
                     except Exception as e:
-                        raise StepError('unexpected vlaue for vacc_count.') from e
+                        raise StepError('unexpected vlaue for vacc_doses.') from e
                     if vacc_name == 'Moderna':
-                        moderna_count += vacc_count
+                        moderna_doses += vacc_doses
                     elif vacc_name == 'AstraZeneca':
-                        astrazeneca_count += vacc_count
+                        astrazeneca_doses += vacc_doses
                     elif vacc_name == 'Janssen':
-                        janssen_count += vacc_count
+                        janssen_doses += vacc_doses
                     elif vacc_name == 'Comirnaty':
-                        comirnaty_count += vacc_count
+                        comirnaty_doses += vacc_doses
                     else:
                         self.logger.info(f'unknown vaccine: {vacc_name}')
                 index += 1
-            dict = {'Moderna': moderna_count, 'AstraZeneca': astrazeneca_count, 'Janssen': janssen_count, 'Comirnaty': comirnaty_count}
+            dict = {'Moderna': moderna_doses, 'AstraZeneca': astrazeneca_doses, 'Janssen': janssen_doses, 'Comirnaty': comirnaty_doses}
             return dict
