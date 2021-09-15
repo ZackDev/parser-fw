084f70d0965f67fab407bfa847d01c14ec555711
ZackDev <bastian.maier@gmx.de>
2021-09-15 19:22:32 +0200

    reworked DailyCasesParser

--- steps/DailyCasesParser.py
+++ steps/DailyCasesParser.py
@@ -19,62 +19,38 @@ class DailyCasesParser(AbstractStep):
             ''' each line after that corresponds to a country, containing the cases among other data '''
             csv_reader = csv.reader(daily_cases_csv, delimiter=',')
             index = 0
-            for line in csv_reader:
-                if index == 0:
-                    raw_dates = line[4:]
-                    self.logger.debug(f'raw_dates: {raw_dates}')
-                elif index > 0:
-                    if line[1] == self.country:
+            while country_found is False:
+                for line in csv_reader:
+                    if index == 0:
+                        raw_dates = line[4:]
+                    elif line[1] == self.country:
                         country_found = True
                         raw_cases = line[4:]
                         self.logger.debug(f'raw_cases: {raw_cases}')
                         break
-                index += 1
+                    index += 1
 
-            if country_found is False:
-                raise StepError(f'country {self.country} not found.')
 
-            if raw_dates and raw_cases:
+            if (raw_dates is not None) & (raw_cases is not None):
                 dates = []
                 cases = []
 
                 ''' fill the dates and cases that are missing with pseudo-data, e.g. 1.1 to 21.1 '''
                 for x in range(1, 22):
-                    if x < 10:
-                        dates.append(f'2020-01-0{x}')
-                    elif x >= 10:
-                        dates.append(f'2020-01-{x}')
+                    dates.append(f'2020-01-{x:02}')
                     cases.append(0)
 
                 ''' do some date parsing, provided format is M/D/Y, to YYYY-MM-DD '''
                 for raw_date in raw_dates:
                     date_array = raw_date.split('/')
                     day, month, year = None, None, None
-                    if len(date_array) == 3:
-                        try:
-                            day = str_to_integer(date_array[1], '+')
-                            month = str_to_integer(date_array[0], '+')
-                            year = str_to_integer(date_array[2], '+')
-                        except Exception as e:
-                            raise StepError(f'error parsing date_array {date_array}') from e
-                    else:
-                        raise StepError('raw_date array length is not 3.')
+                    try:
+                        day = f"{str_to_integer(date_array[1], '+'):02}"
+                        month = f"{str_to_integer(date_array[0], '+'):02}"
+                        year = f"20{str_to_integer(date_array[2], '+')}"
+                    except Exception as e:
+                        raise StepError(f'error parsing date_array {date_array}') from e
 
-                    if day >= 1 and day < 10:
-                        day = f'0{day}'
-                    elif day >= 10 and day <= 31:
-                        day = f'{day}'
-                    else:
-                        raise StepError('day not in expected range.')
-
-                    if month >= 1 and month < 10:
-                        month = f'0{month}'
-                    elif month >= 10 and month <= 12:
-                        month = f'{month}'
-                    else:
-                        raise StepError('month not in expected range.')
-
-                    year = f'20{year}'
                     date = f'{year}-{month}-{day}'
                     dates.append(date)
                     self.logger.debug(f'appended date: {date}')
@@ -107,11 +83,10 @@ class DailyCasesParser(AbstractStep):
             if len(dates) != len(cases):
                 raise StepError('dates and cases array length not equal.')
 
-            if len(dates) < 1 and len(cases) < 1:
+            elif len(dates) < 1 and len(cases) < 1:
                 raise StepError('dates and cases array length is zero.')
 
-            dates_is_valid = is_valid_ISO8601_date_array(dates, True)
-            if dates_is_valid is False:
+            elif is_valid_ISO8601_date_array(dates, True) is False:
                 raise StepError('date array is inconsistent.')
 
             dict = {"dates": dates, "cases": cases}
