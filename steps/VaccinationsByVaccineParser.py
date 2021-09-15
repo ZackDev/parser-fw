38d302a88c031e41f98dee40caddb29e08201cad
ZackDev <bastian.maier@gmx.de>
2021-09-15 14:26:34 +0200

    refactoring

--- steps/VaccinationsByVaccineParser.py
+++ steps/VaccinationsByVaccineParser.py
@@ -6,37 +6,29 @@ import csv
 
 class VaccinationsByVaccineParser(AbstractStep):
     def run(self, data):
-        dict = None
+        dict = {}
         with StringIO(data.decode('utf-8')) as vaccine_csv:
             csv_reader = csv.reader(vaccine_csv, delimiter=',')
-            moderna_doses = 0
-            astrazeneca_doses = 0
-            janssen_doses = 0
-            comirnaty_doses = 0
 
             ''' NOTE: the first line is the header '''
             index = 0
             for line in csv_reader:
-                if index > 0:
+                if index == 0:
+                    pass
+                else:
                     vacc_name = line[2]
                     vacc_doses = line[4]
                     try:
                         vacc_doses = str_to_integer(vacc_doses, '+')
                     except Exception as e:
-                        raise StepError('unexpected vlaue for vacc_doses.') from e
-                    if vacc_name == 'Moderna':
-                        moderna_doses += vacc_doses
-                    elif vacc_name == 'AstraZeneca':
-                        astrazeneca_doses += vacc_doses
-                    elif vacc_name == 'Janssen':
-                        janssen_doses += vacc_doses
-                    elif vacc_name == 'Comirnaty':
-                        comirnaty_doses += vacc_doses
-                    else:
-                        self.logger.info(f'unknown vaccine: {vacc_name}')
+                        raise StepError('unexpected value for vacc_doses.') from e
+                    try:
+                        doses = dict[vacc_name]
+                        dict.update({vacc_name: doses + vacc_doses})
+                    except KeyError:
+                        dict.update({vacc_name: vacc_doses})
                 index += 1
-            dict = {'Moderna': moderna_doses, 'AstraZeneca': astrazeneca_doses, 'Janssen': janssen_doses, 'Comirnaty': comirnaty_doses}
-        if dict is not None:
+        if len(dict) > 0:
             return dict
         else:
-            raise StepError('unexpected error: dict is None.')
+            raise StepError('unexpected error: dict is empty.')
