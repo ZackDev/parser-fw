a3bb95c52a82e479db8a6cd2b92a4e049ce0ff32
ZackDev <bastian.maier@gmx.de>
2021-09-15 13:58:56 +0200

    refactoring

--- steps/VaccinationsByVaccineParser.py
+++ steps/VaccinationsByVaccineParser.py
@@ -6,9 +6,9 @@ import csv
 
 class VaccinationsByVaccineParser(AbstractStep):
     def run(self, data):
+        dict = None
         with StringIO(data.decode('utf-8')) as vaccine_csv:
             csv_reader = csv.reader(vaccine_csv, delimiter=',')
-            dict = None
             moderna_doses = 0
             astrazeneca_doses = 0
             janssen_doses = 0
@@ -36,4 +36,7 @@ class VaccinationsByVaccineParser(AbstractStep):
                         self.logger.info(f'unknown vaccine: {vacc_name}')
                 index += 1
             dict = {'Moderna': moderna_doses, 'AstraZeneca': astrazeneca_doses, 'Janssen': janssen_doses, 'Comirnaty': comirnaty_doses}
+        if dict is not None:
             return dict
+        else:
+            raise StepError('unexpected error: dict is None.')
