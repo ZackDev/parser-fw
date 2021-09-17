40a3ce19b053ddf17ca3aa0188f4df90463acaca
ZackDev <bastian.maier@gmx.de>
2021-09-17 09:53:25 +0200

    cleanup

--- Seshu.py
+++ Seshu.py
@@ -11,19 +11,15 @@ class SeshuError(Exception):
 
 
 class Seshu:
-    def __init__(self, factory: AbstractSequenceProviderAndRunnerFactory, loglevel: int = _DEFAULT_LOGLEVEL):
-        loglevel = loglevel*10
+    def __init__(self, factory: AbstractSequenceProviderAndRunnerFactory, loglevel: int = logging.INFO):
+        loglevel = loglevel * 10
         # loglevels in numbers: DEBUG:10, INFO:20, WARN:30, ERROR:40, CRITICAL:50
-        available_loglevels = [logging.DEBUG, logging.INFO,
-                               logging.WARN, logging.ERROR, logging.CRITICAL]
-        cnt = available_loglevels.count(loglevel)
-        if cnt != 1:
+        if loglevel not in [logging.DEBUG, logging.INFO, logging.WARN, logging.ERROR, logging.CRITICAL]:
             loglevel = logging.INFO
         logging.basicConfig(filename='parser-fw.log', encoding='utf-8', level=loglevel, format='%(asctime)s %(name)s %(levelname)s : %(message)s')
         logger = logging.getLogger(__name__)
         logger.info(f'program started with sequence_name: {factory.sequence_name} and loglevel: {loglevel}.')
 
-        """ create SequenceProvider """
         try:
             sequence_provider = factory.get_provider()
             sequence_runner = factory.get_runner()
@@ -31,14 +27,12 @@ class Seshu:
             for step in steps:
                 sequence_runner.add_step(step)
             sequence_runner.run()
-            logger.info(
-                f'finished sequence {factory.sequence_name} ({len(sequence_runner.steps)} steps).')
+            quantifier = 'step' if len(sequence_runner.steps) == 1 else quantifier = 'steps'
+            logger.info(f'finished sequence {factory.sequence_name} ({len(sequence_runner.steps)} {quantifier}).')
         except SequenceProviderError:
-            logging.exception(
-                f'error creating SequenceProvider object with sequence_name: {factory.sequence_name}.')
+            logging.exception(f'error creating SequenceProvider object with sequence_name: {factory.sequence_name}.')
         except SequenceRunnerError:
-            logging.exception(
-                f'error running sequence: {factory.sequence_name}')
+            logging.exception(f'error running sequence: {factory.sequence_name}')
         except Exception:
             logging.exception('unexpected error.')
         finally:
