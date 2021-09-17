f6a938e632991c54e4044ded7423218fb8048335
ZackDev <bastian.maier@gmx.de>
2021-09-17 17:00:55 +0200

    adjusted test range

--- test/ConvertersTest.py
+++ test/ConvertersTest.py
@@ -5,49 +5,43 @@ from misc.Converters import str_to_integer
 class ConvertersTest(unittest.TestCase):
 
     def test_str_to_integer(self):
-        with self.subTest():
-            self.assertEqual(str_to_integer('123'), 123)
+        self.assertEqual(str_to_integer('123'), 123)
 
-        with self.subTest():
-            self.assertEqual(str_to_integer('-123', '-'), -123)
+        self.assertEqual(str_to_integer('-123', '-'), -123)
 
-        with self.subTest():
-            self.assertEqual(str_to_integer('999999999'), 999999999)
+        self.assertEqual(str_to_integer('999999999'), 999999999)
 
-        with self.subTest():
-            with self.assertRaises(ValueError):
-                str_to_integer('123', '-')
+        self.assertEqual(str_to_integer('0000'), 0)
 
-        with self.subTest():
-            with self.assertRaises(TypeError):
-                str_to_integer(123)
+        with self.assertRaises(ValueError):
+            str_to_integer('123', '-')
 
-        with self.subTest():
-            with self.assertRaises(ValueError):
-                str_to_integer('abcd')
+        with self.assertRaises(ValueError):
+            str_to_integer('abcd')
 
-        with self.subTest():
-            with self.assertRaises(TypeError):
-                str_to_integer(0.23)
+        with self.assertRaises(TypeError):
+            str_to_integer(0.23)
 
-        with self.subTest():
-            with self.assertRaises(TypeError):
-                str_to_integer(-1.0)
+        with self.assertRaises(TypeError):
+            str_to_integer(-1.0)
 
-        with self.subTest():
-            with self.assertRaises(TypeError):
-                str_to_integer(True)
-            with self.assertRaises(TypeError):
-                str_to_integer(False)
+        with self.assertRaises(TypeError):
+            str_to_integer(True)
+
+        with self.assertRaises(TypeError):
+            str_to_integer(False)
+
+        with self.assertRaises(TypeError):
+            str_to_integer(123)
+
+        with self.assertRaises(TypeError):
+            str_to_integer(None)
 
         for i in range(0, 999999):
-            with self.subTest():
-                self.assertEqual(str_to_integer(str(i), '+'), i)
+            self.assertEqual(str_to_integer(str(i), '+'), i)
 
         for i in range(-999999, 0):
-            with self.subTest():
-                self.assertEqual(str_to_integer(str(i), '-'), i)
+            self.assertEqual(str_to_integer(str(i), '-'), i)
 
         for i in range(-499999, 499999):
-            with self.subTest():
-                self.assertEqual(str_to_integer(str(i), '*'), i)
+            self.assertEqual(str_to_integer(str(i), '*'), i)
