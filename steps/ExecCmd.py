4b7ea0baa34532735fc5e2c14d3dbf486fcfd241
ZackDev <ZackDev@users.noreply.github.com>
2022-01-12 13:21:37 +0100

    getting and changing directory back to cwd

--- steps/ExecCmd.py
+++ steps/ExecCmd.py
@@ -4,10 +4,14 @@ import os
 
 class ExecCmd(AbstractStep):
     def run(self, data):
-        pexists = os.path.exists(self.path)
-        if pexists is True:
+        # get current working directory
+        cwd = os.getcwd()
+        if os.path.exists(self.path) is True:
+            # change directory, run commands
             os.chdir(self.path)
             for cmd in self.cmds:
                 os.system(cmd)
+            # change directory back to cwd
+            os.chdir(cwd)
         else:
             raise StepError(f'path: {self.path} not found')
