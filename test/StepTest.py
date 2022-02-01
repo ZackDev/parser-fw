a59b4527708995d15480cfbe4c38a37c2d8b28ba
ZackDev <ZackDev@users.noreply.github.com>
2022-02-01 14:09:36 +0100

    removed wrong assertion

--- test/StepTest.py
+++ test/StepTest.py
@@ -11,7 +11,6 @@ class StepTest(unittest.TestCase):
     def test_empty_kwargs(self):
         attr = {}
         c = CustomTest(**attr)
-        self.assertRaises(AttributeError)
 
     def test_no_kwargs(self):
         c = CustomTest()
